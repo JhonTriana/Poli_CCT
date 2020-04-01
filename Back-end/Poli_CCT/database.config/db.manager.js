e15de671ec174a90f4f83937fb589e23fe475315
javargasgue <55053065+javargasgue@users.noreply.github.com>
2020-03-31 23:40:22 -0500

    Incluir modelos
    criterios, documentos, actividades, ver video

--- Back-end/Poli_CCT/database.config/db.manager.js
+++ Back-end/Poli_CCT/database.config/db.manager.js
@@ -4,13 +4,26 @@ const Sequelize = require("sequelize");
 const sequelizeConnection = require('../database.config/db.connection.js');
 //IMPORT MODELS
 const UserModel = require("../model/user.model");
+const ActividadModel = require("../model/actividades.model");
+const CriterioModel = require("../model/criterios.model");
+const VerVideoModel = require("../model/verVideo.model");
+const DocumentoModel = require("../model/documentos.model");
 //INITIALIZE MODELS
 const User = UserModel (sequelizeConnection, Sequelize);
+const Actividad = ActividadModel (sequelizeConnection, Sequelize);
+const Criterio = CriterioModel (sequelizeConnection, Sequelize);
+const VerVideo = VerVideoModel (sequelizeConnection, Sequelize);
+const Documento = DocumentoModel (sequelizeConnection, Sequelize);
+
 //CREATE RELATIONS BETWEEN MODELS
 //Post.belongsTo( User, { foreignKey: 'idUser', sourceKey: 'idPost' });
 //GROUP MODELS
 const models = {
   User: User,
+  Actividad: Actividad,
+  Criterio: Criterio,
+  VerVideo: VerVideo,
+  Documento: Documento
 };
 /**
  * Create object to manage the models and database
