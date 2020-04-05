b5baa4786a4c1646ec90d0bef00a380140aabdbb
Wimarinr <61330239+Wimarinr@users.noreply.github.com>
2020-04-04 23:01:25 -0500

    Create sede.model.js

---
+++ Back-end/Poli_CCT/model/sede.model.js
@@ -0,0 +1,16 @@
+module.exports = (sequelize, Sequelize) =>{
+    const sede = sequelize.define ("sede", {
+        idSede: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true 
+        },
+        idID: { type: Sequelize.INTEGER }, 
+        idLugarEjecucion: { type: Sequelize.INTEGER },
+        idIDSede: { type: Sequelize.INTEGER }
+
+    }, {
+        tableName: "sedes"
+    });    
+    
+    return sede;
