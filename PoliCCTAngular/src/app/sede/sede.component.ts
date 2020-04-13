bffe2f92324ad30f502ec5a27b32a174f1a02f79
Wimarinr <61330239+Wimarinr@users.noreply.github.com>
2020-04-12 22:58:33 -0500

    [wimarinrl] Formularios sede y ubicacion

---
+++ PoliCCTAngular/src/app/sede/sede.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sede',
+  templateUrl: './sede.component.html',
+  styleUrls: ['./sede.component.scss']
+})
+export class SedeComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
