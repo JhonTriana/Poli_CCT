2aed1f6501abf999fa3c69d94a2287f02094ea9e
andres felipe <andres100r@hotmail.com>
2020-04-17 19:03:44 -0500

    login

--- PoliCCTAngular/src/app/login/login.component.ts
+++ PoliCCTAngular/src/app/login/login.component.ts
@@ -1,21 +1,4 @@
-import { Component, OnInit } from '@angular/core';
 
-@Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
-})
-export class LoginComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
-
-
-/*
 
 
 
@@ -46,4 +29,3 @@ export class LoginComponent implements OnInit {
   }
 }
 
-*/
