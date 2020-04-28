e3760dc69ab14a297a056177f0390205f1bb269c
Julian Andres Vargas Guerrero <julius_javg_01@hotmail.com>
2020-04-27 20:01:53 -0500

    [JaVargasGue] Crear Ventana Emergente

--- PoliCCTAngular/src/app/actividad/actividad.component.ts
+++ PoliCCTAngular/src/app/actividad/actividad.component.ts
@@ -3,35 +3,33 @@ import { Actividad } from '../models/actividades.model';
 import { ActividadService } from '../services/actividad.service';
 import { MatTableDataSource } from '@angular/material';
 import { MatPaginator } from '@angular/material/paginator';
-import { MatDialog, MatDialogRef, MAT_DIALOG_DATA} from '@angular/material/dialog';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-actividad',
   templateUrl: './actividad.component.html',
   styleUrls: ['./actividad.component.scss']
 })
-
 export class ActividadComponent implements OnInit {
 
   @ViewChild(MatPaginator, {static: true}) paginator: MatPaginator;
-  newActividad: Actividad;
   misActividades ; 
-  No = 4 ; 
+  newActividad: Actividad;
+  
   constructor(private ActividadService: ActividadService , public dialog: MatDialog ) { 
-    this.newActividad = new Actividad;
     this.getAllActividades();
     this.dataSource = new MatTableDataSource(this.misActividades);
+    this.newActividad = new Actividad;
   }
-  nuevaActividad(){
-    if (this.newActividad.nombreActividad === undefined ){
-    }
-    else{
-      this.newActividad.idActividad = this.No ; 
-      this.ActividadService.createNewActividad(this.newActividad);
-      this.No = this.No + 1 ;
-      this.newActividad = new Actividad;
-      this.getAllActividades();
-    }
+  openDialog(): void {
+    const dialogRef = this.dialog.open(VentanaEmergente, {
+      width: '300px',
+      data: {idActividad: this.newActividad.idActividad , nombreActividad: this.newActividad.nombreActividad}
+    });
+    dialogRef.afterClosed().subscribe(result => {
+      console.log('The dialog was closed');
+      //this.animal = result;
+    });
   }
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
@@ -39,14 +37,54 @@ export class ActividadComponent implements OnInit {
   getAllActividades(){
     this.ActividadService.getAllActividades().subscribe(   misActividadesObs => {   this.misActividades = misActividadesObs;   }   )
     this.dataSource = new MatTableDataSource(this.misActividades);
-  }
-  
+  } 
   displayedColumns: string[] = ['idActividad', 'nombreActividad', "star"];
   dataSource = new MatTableDataSource(this.misActividades);
   applyFilter(event: Event) {
     const filterValue = (event.target as HTMLInputElement).value;
     this.dataSource.filter = filterValue.trim().toLowerCase();
   }
-  openDialog(): void {   
+}
+@Component({
+  selector: 'actividad.Emergente',
+  templateUrl: 'actividad.Emergente.html',
+})
+
+
+export class VentanaEmergente {
+  misActividades ; 
+  newActividad: Actividad;
+  No = 4 ; 
+  
+  constructor(private ActividadService: ActividadService , public dialogRef: MatDialogRef<VentanaEmergente>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {
+      this.newActividad = new Actividad;
+      this.dataSource = new MatTableDataSource(this.misActividades);
+      this.newActividad = new Actividad;  
+    }
+  onNoClick(): void {
+    this.dialogRef.close();
   }
+  nuevaActividad(){    
+    if (this.newActividad.nombreActividad === undefined ){
+    }
+    else{
+      this.newActividad.idActividad = this.No ; 
+      this.ActividadService.createNewActividad(this.newActividad);
+      this.No = this.No + 1 ;
+      this.newActividad = new Actividad;
+      this.dialogRef.close();
+    }
+  }
+  getAllActividades(){
+    this.ActividadService.getAllActividades().subscribe(   misActividadesObs => {   this.misActividades = misActividadesObs;   }   )
+    this.dataSource = new MatTableDataSource(this.misActividades);
+  }
+  displayedColumns: string[] = ['idActividad', 'nombreActividad', "star"];
+  dataSource = new MatTableDataSource(this.misActividades);
+  
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  } 
 }
