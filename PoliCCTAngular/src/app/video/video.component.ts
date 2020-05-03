6b772009319399842158128841ef31985181a0a2
andres felipe <andres100r@hotmail.com>
2020-05-02 22:11:30 -0500

    login video

--- PoliCCTAngular/src/app/video/video.component.ts
+++ PoliCCTAngular/src/app/video/video.component.ts
@@ -30,19 +30,13 @@ export class VideoComponent implements OnInit {
 
   constructor(private router: Router) { }
   
-  username: string;
-  password: string;
+  
 
   ngOnInit(): void {
   }
   
-  login() : void {
-    if(this.username == 'admin' && this.password == 'admin'){
-     this.router.navigate(["actividad"]);
-    }else {
-      alert("Invalid credentials");
-    }
-
-    
-  }
+ 
 }
+
+
+
