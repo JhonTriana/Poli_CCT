1e40cb75abe4a78ee5d1b656afdec987359e0810
Julian Andres Vargas Guerrero <julius_javg_01@hotmail.com>
2020-05-20 19:11:35 -0500

    [JaVargasGue] Complementar Video

--- PoliCCTAngular/src/app/video/video.component.ts
+++ PoliCCTAngular/src/app/video/video.component.ts
@@ -1,44 +1,25 @@
-/*import { Component, OnInit } from '@angular/core';
-
-@Component({
-  selector: 'app-video',
-  templateUrl: './video.component.html',
-  styleUrls: ['./video.component.scss']
-})
-export class VideoComponent implements OnInit {
-  panelOpenState = false;
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
-*/
-
-
-
-
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 
-
-
 @Component({
   selector: 'app-video',
   templateUrl: './video.component.html',
   styleUrls: ['./video.component.scss']
-})
+})  
 export class VideoComponent implements OnInit {
 
-  constructor(private router: Router) { }
-  
-  
+  checked = false;
+  disabled = true ;
+  milisegundos = 15000;
 
+  constructor(private router: Router) {
+  }
   ngOnInit(): void {
   }
-  
  
 }
-
-
-
+  
+setTimeout(ActivarBoton, 15000 );
+function ActivarBoton(){
+  this.disabled = false ;
+}
